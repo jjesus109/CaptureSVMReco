0b1f43524df27a27c3ebdcb6743b59ae1be146b6
jjesus109 <jesjalher@gmail.com>
2018-09-17 03:17:47 +0000

    se añade archivo ejectuable de rasp

---
+++ iniciar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /home/pi/.virtualenvs/cv/bin/activate
+cd /home/pi/Desktop/P2/CaptureSVMReco
+git pull
+#python ScriptPrincipal.py
+#python reconocimientoSVM_Live_10tomas.py
+
+
+
