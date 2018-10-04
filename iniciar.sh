cc1adfc9e80b827f940da22b05687765980ced80
jjesus109 <jesjalher@gmail.com>
2018-10-04 09:32:48 -0500

    Prueba pir y modificacion de termo de queues

--- iniciar.sh
+++ iniciar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-source /home/pi/.virtualenvs/cv/bin/activate
+#source /home/pi/.virtualenvs/cv/bin/activate
 cd /home/pi/Desktop/P2/CaptureSVMReco
 git pull
 #modificacion en mi compu
-#python ScriptPrincipal.py
-python prueba_PIR.py
+python ScriptPrincipal.py
+
 #python reconocimientoSVM_Live_10tomas.py
 
 
