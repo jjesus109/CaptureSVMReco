cedbe13ac4156a5941fddc1db7becee2c2df49fd
jjesus109 <jesjalher@gmail.com>
2018-10-11 10:37:21 -0500

    Prueba queues para catura de rostros

--- iniciar.sh
+++ iniciar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /home/pi/.virtualenvs/cv/bin/activate
 cd /home/pi/Desktop/P2/CaptureSVMReco
-git pull
+git pull origin master --allow-unrelated-histories
 #modificacion en mi compu
 python ScriptPrincipal.py
 
