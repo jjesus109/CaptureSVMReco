face0f874fbb1795714d0d8763e9c79b201bccc5
jjesus109 <jesjalher@gmail.com>
2018-10-11 09:14:02 -0500

    Prueba queues para catura de rostros

--- README.md
+++
@@ -1,2 +0,0 @@
-# CaptureSVMReco
-Proyecto para extraer, entrenar y clasificar rostros por medio de clasificadores de Haar y una SVM
--- prueba_PIR.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import MotionSensor
-pir = MotionSensor(4) # Numero de
-
-while True:
-#    pir.when_motion = rL.reconocimiento(db)
-    if pir.motion_detected:
-        print("En ell if del pir")
