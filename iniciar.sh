be3727692b261de661b1c54bea1245e2eec57ce5
jjesus109 <jesjalher@gmail.com>
2018-11-10 12:29:42 -0600

    pruebas finales en entorno final

--- iniciar.sh
+++ iniciar.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-source /home/pi/.virtualenvs/cv/bin/activate
+source /home/pi/.virtualenvs/cv2/bin/activate
 cd /home/pi/Desktop/P2/CaptureSVMReco
 git pull origin master --allow-unrelated-histories
 #modificacion en mi compu
-python ScriptPrincipal.py
+python verImpresora.py
+#python ScriptPrincipal.py
 
 #python reconocimientoSVM_Live_10tomas.py
-
-
-
