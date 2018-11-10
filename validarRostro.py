511ea3ab6fe5e96c1f4a036fb43560f3da6cf7a5
jjesus109 <jesjalher@gmail.com>
2018-11-10 17:37:17 -0600

    pruebas finales en entorno final

--- validarRostro.py
+++ validarRostro.py
@@ -52,7 +52,7 @@ def filtrar(carpeta):
                 os.remove(Rimagen)
             else:
                  tamanioCara = np.shape(crop_img)
-                 if tamanioCara[0] > resizeW:
+                 if tamanioCara[0] >int(resizeW*0.83):
         #        crop_img = cv2.resize(crop_img,(resizeW,resizeH))
                      crop_img = cv2.resize(crop_img,(resizeW,resizeH))
                      cv2.imwrite(Rimagen, crop_img)
