80ccf52345a71b9e505c9a6a6d0707f8fb289f1b
jjesus109 <jesjalher@gmail.com>
2018-11-10 17:29:09 -0600

    pruebas finales en entorno final

--- capturaRostrosQueues.py
+++ capturaRostrosQueues.py
@@ -14,16 +14,16 @@ def _detect_(inputQueue, outputQueue):
             gray = inputQueue.get()
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
             for (x, y, w, h) in faces:
-#                medidasX1 = int(x*1.135)
-#                medidasX2 = int(x+(w*0.82))
-#                medidasY2 = int(y*1.2)
-#                medidasY1 = int(y+(h*0.95))
-                medidasX1 = int(x*1.4)
-                medidasX2 = int(x+(w*0.9))
-                medidasY2 = int(y*1.25)
-                medidasY1 = int(y+(h*0.98))
-#                vectorDim = [x,y+h,x+w,y] 
-                vectorDim = [medidasX1,medidasY1,medidasX2,medidasY2] 
+                medidasX1 = int(x*1.135)
+                medidasX2 = int(x+(w*0.82))
+                medidasY2 = int(y*1.2)
+                medidasY1 = int(y+(h*0.95))
+#                medidasX1 = int(x*1.4)
+#                medidasX2 = int(x+(w*0.9))
+#                medidasY2 = int(y*1.25)
+#                medidasY1 = int(y+(h*0.98))
+                vectorDim = [x,y+h,x+w,y] 
+#                vectorDim = [medidasX1,medidasY1,medidasX2,medidasY2] 
                 outputQueue.put(vectorDim)
 
 # Funcion de ajuste Gamma
