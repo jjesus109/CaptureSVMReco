17a765131f0d7627dd5f669265efdefa9e2a8d7d
jjesus109 <jesjalher@gmail.com>
2018-11-10 21:29:33 -0600

    pruebas finales en entorno final

--- recog_queues.py
+++ recog_queues.py
@@ -17,20 +17,21 @@ import pickle
 
 
 def detect(inputQueue, outputQueue):
+#    crop_img=np.array([0,0,0])
     while True:
         if not inputQueue.empty():
             gray = inputQueue.get()
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 #            print("Aqui en faces")
             for (x, y, w, h) in faces:
+                medidasX1 = int(x*1.4)                
+                medidasX2 = int(x+(w*0.9))
+                medidasY2 = int(y*1.25)
+                medidasY1 = int(y+(h*0.98))
+#        cv2.rectangle(frame, (medidasX1, medidasY1), (medidasX2, medidasY2), (255, 0, 0), 2)
                 
-#                medidasX1 = int(x*1.135)
-#                medidasX2 = int(x+(w*0.82))
-#                medidasY2 = int(y*1.2)
-#                medidasY1 = int(y+(h*0.95))
-##                vectorDim = [medidasX1,medidasY1,medidasX2,medidasY2] 
-#                cv2.rectangle(frame, (medidasX1, medidasY1), (medidasX2, medidasY2), (255, 0, 0), 2)
-                vectorDim = [x,y+h,x+w,y] 
+                vectorDim = [medidasX1,medidasY1,medidasX2,medidasY2] 
+#                vectorDim = [x,y+h,x+w,y] 
                 outputQueue.put(vectorDim)
                 
                 
@@ -123,6 +124,7 @@ def reconocimiento(db,llamada,indexCamara, p, inputQueue, outputQueue):
     print("Se comunico con camara:" +str(video_capture.isOpened()))
     if video_capture.isOpened():
 #        ledes.on()
+        n=1
         while True:
             _, frame = video_capture.read()
         
@@ -141,11 +143,14 @@ def reconocimiento(db,llamada,indexCamara, p, inputQueue, outputQueue):
                 crop_img = Clahe_Gamma[medidasY2:medidasY1, medidasX1:medidasX2]
                 tamanioCara = np.shape(crop_img)
                 if tamanioCara[0] >int(resizeW*0.7):
+                    cv2.imwrite(str(n),crop_img)
+                    n += 1
                     crop_img = cv2.resize(crop_img,(resizeW,resizeH))
                     imagenFlatten = crop_img.ravel()
                     imagenLista = imagenFlatten.tolist()
                     listaImagenes.append(imagenLista)
                     if len(listaImagenes)==10:
+                        n = 0
                         matrizImagenes= np.asarray(listaImagenes)
                         prueba_pca = pca.transform(matrizImagenes)
                         probabilidades = clf.predict_proba(prueba_pca)
