2e981a903f2dacc0ba7ed4d49b279862e39b070b
jjesus109 <jesjalher@gmail.com>
2018-11-18 21:57:41 -0600

    ajuste guarda capturas

--- capturaRostrosQueues.py
+++ capturaRostrosQueues.py
@@ -89,13 +89,13 @@ def capturaCamara(NombreCarpetaPrueba,numeroUsuarios, llamada,p, inputQueue, out
             Clahe_Gamma = clahe.apply(CorreccionGamma)
             
 
-            CGamma = ajusteGamma(frame,1.8)
-            clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
-            lab = cv2.cvtColor(CGamma, cv2.COLOR_BGR2LAB)
-            lab_planes = cv2.split(lab)
-            lab_planes[0] = clahe.apply (lab_planes[0])
-            lab = cv2.merge(lab_planes)
-            bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+#            CGamma = ajusteGamma(frame,1.8)
+#            clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
+#            lab = cv2.cvtColor(CGamma, cv2.COLOR_BGR2LAB)
+#            lab_planes = cv2.split(lab)
+#            lab_planes[0] = clahe.apply (lab_planes[0])
+#            lab = cv2.merge(lab_planes)
+#            bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
 #            Clahe_Gamma = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
 #                cv2.imshow("CAPTURA",Clahe_Gamma)
             if inputQueue.empty():
@@ -104,8 +104,8 @@ def capturaCamara(NombreCarpetaPrueba,numeroUsuarios, llamada,p, inputQueue, out
                 vectorDim = outputQueue.get()
             if vectorDim !=[0,0,0,0]:
                 medidasX1,medidasY1,medidasX2,medidasY2 = vectorDim
-                cv2.rectangle(bgr, (medidasX1, medidasY1), (medidasX2, medidasY2), (255, 0, 0), 2)
-                crop_img = bgr[medidasY2:medidasY1, medidasX1:medidasX2]
+                cv2.rectangle(frame, (medidasX1, medidasY1), (medidasX2, medidasY2), (255, 0, 0), 2)
+                crop_img = frame[medidasY2:medidasY1, medidasX1:medidasX2]
     #                crop_img = cv2.resize(crop_img,(resizeW,resizeH))
                 # Para evitar que devuelve basura en este caso un entero cuando 
                 # no reconcoe algun rostro
