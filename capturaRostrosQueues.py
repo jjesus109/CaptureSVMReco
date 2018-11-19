6619179285ef4f207c19d0f1a77b622638872091
jjesus109 <jesjalher@gmail.com>
2018-11-18 22:09:58 -0600

    ajuste guarda capturas

--- capturaRostrosQueues.py
+++ capturaRostrosQueues.py
@@ -105,7 +105,7 @@ def capturaCamara(NombreCarpetaPrueba,numeroUsuarios, llamada,p, inputQueue, out
             if vectorDim !=[0,0,0,0]:
                 medidasX1,medidasY1,medidasX2,medidasY2 = vectorDim
                 cv2.rectangle(bgr, (medidasX1, medidasY1), (medidasX2, medidasY2), (255, 0, 0), 2)
-                crop_img = bgr[medidasY2:medidasY1, medidasX1:medidasX2]
+                crop_img = Clahe_Gamma[medidasY2:medidasY1, medidasX1:medidasX2]
     #                crop_img = cv2.resize(crop_img,(resizeW,resizeH))
                 # Para evitar que devuelve basura en este caso un entero cuando 
                 # no reconcoe algun rostro
