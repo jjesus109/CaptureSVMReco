e6ebdbfd223baeac840edc613d44de11d4739771
jjesus109 <jesjalher@gmail.com>
2018-11-25 20:54:50 -0600

    Mod

--- svm_pca_final.py
+++ svm_pca_final.py
@@ -159,6 +159,8 @@ def SVM(carpeta,target_names, numeroMuestrasRostros):
 #    y_pred_probas = clf.predict(X_test_pca)
     
     import pickle
+    print("Estos son los target a guardar")
+    print(target_names)
     datos = {"modelo":clf, "pca": pca, "target_names": target_names,"NombreCarpetaPrueba":carpeta}
     data = open("archivo_modelo_LBP.pickle",'wb')
     pickle.dump(datos, data)
