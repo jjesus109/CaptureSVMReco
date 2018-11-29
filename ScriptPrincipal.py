a7e02ff95593d520d43e2e75a8f83bcf7765060c
jjesus109 <jesjalher@gmail.com>
2018-11-29 16:10:32 -0600

    Se agregan mensajes de errores y modo para detener el programa en cualquier momento

--- ScriptPrincipal.py
+++ ScriptPrincipal.py
@@ -137,54 +137,149 @@ estadoActualPuerta = False
 
 nombre = "Sin reconocer"
 conexionExitosa,firebase,db, valores,_ = conectarFirebase()
-extraccion = False
-while True:
+
+def funcionPrincipal():
+    extraccion = False
+    indexCamara = 0
+    while True:
+        
+        numeroMuestrasRostros = 60
+        configurado = db.child("Facial/Configurado").get()
+        configurado = configurado.val()
     
-    numeroMuestrasRostros = 60
-    configurado = db.child("Facial/Configurado").get()
-    configurado = configurado.val()
-
-    if configurado==True:
-
-        numeroUsuarios = db.child("Facial/NumeroUsuarios").get()
-        numeroUsuarios = numeroUsuarios.val()
-        detener= db.child("Facial/Detener").get()
-        if detener.val() == True:
-            break
-        if extraccion == False:
-            try:
-                print("Esta configurado")
-                
+        if configurado==True:
+    
+            numeroUsuarios = db.child("Facial/NumeroUsuarios").get()
+            numeroUsuarios = numeroUsuarios.val()
+            detener= db.child("Facial/Detener").get()
+            if detener.val() == True:
+                break
+            if extraccion == False:
+                try:
+                    print("Esta configurado")
+                    
+                    tomaDatos = open("archivo_modelo_LBP.pickle", "rb")
+                    datos = pickle.load(tomaDatos)
+                    clf = datos["modelo"]
+                    pca = datos["pca"]
+                    target_names =datos["target_names"]
+                    NombreCarpetaPrueba = datos["NombreCarpetaPrueba"]
+        
+                    im_en = rg.encode(NombreCarpetaPrueba, numeroUsuarios)
+                    extraccion = True
+                    print("Extracciòn de modelo realizado correctamente")
+                    
+                    db.child("Facial").update({"Error":"NoErrorExtract"})
+                except:
+                    print("Fallo en la extracción del modelo")
+                    db.child("Facial").update({"Error":"Extract"})
+                    
+            
+            
+    
+            #Validacion para entrenar mas usuarios
+            if len(target_names)<numeroUsuarios:
+                print("se va a añadir un nuevo usuario")
+                NombresEtiquetas={}
+                for i in range(len(target_names)):
+                    NombresEtiquetas[i+1] = target_names[i]
+                #Obtencion de rostros fatantes
+                try:
+                    errorObtencion, NombreCarpetaPrueba, nombreUsuarios, NombresEtiquetas, video_capture, indexCamara,numeroMuestrasRostros= obtenerRostros(indexCamara, list(target_names),len(target_names)+1,NombresEtiquetas)
+                # Envio de mensaje de error <-------------------
+                    db.child("Facial").update({"Error":"NoErrorCaptura"})    
+                    
+                except:
+                    
+                    print("Fallo en metodo de obtencion de rostros")
+                    # Envio de mensaje de error <-------------------
+                    db.child("Facial").update({"Error":"Captura"})    
+                    
+                detener= db.child("Facial/Detener").get()
+                if detener.val() == True:
+                    break
+                    
+                # Entrenamiento de rostros
+                vR.filtrar(NombreCarpetaPrueba,numeroUsuarios)
+    #            NombreCarpetaPrueba = "/home/pi/Desktop/P2/CaptureSVMReco/"
+                detener= db.child("Facial/Detener").get()
+                if detener.val() == True:
+                    break
+                svm.SVM(NombreCarpetaPrueba,nombreUsuarios,numeroMuestrasRostros)
+                detener= db.child("Facial/Detener").get()
+                if detener.val() == True:
+                    break
+                print("Extraccion de modelo con nuevos usuarios")
                 tomaDatos = open("archivo_modelo_LBP.pickle", "rb")
                 datos = pickle.load(tomaDatos)
                 clf = datos["modelo"]
                 pca = datos["pca"]
-                target_names =datos["target_names"]
+                target_names = datos["target_names"]
                 NombreCarpetaPrueba = datos["NombreCarpetaPrueba"]
     
-                im_en = rg.encode(NombreCarpetaPrueba, numeroUsuarios)
-                extraccion = True
-                print("Extracciòn de modelo realizado correctamente")
-                
-                db.child("Facial").update({"Error":"NoErrorExtract"})
-            except:
-                print("Fallo en la extracción del modelo")
-                db.child("Facial").update({"Error":"Extract"})
-                
+                im_en = rg.encode(NombreCarpetaPrueba,numeroUsuarios)
+            
+            print("Clasificación de rostros")
+            # Obtener Discriminantes
+            usuariosActivados = db.child("Facial/UsuariosActivados").get()
+            usuariosActivados = usuariosActivados.val()
+            usuariosActivados = list(usuariosActivados)
+            discriminantes=[]
+            target_names = list(target_names)
+            # Comparacion de metodos
+            for i in target_names:
+                if i not in usuariosActivados:
+                    discriminantes.append(i)
+            if pir.motion_detected == True and (nombre =="Desconocido" or nombre == "Sin reconocer"):
+                detener= db.child("Facial/Detener").get()
+                if detener.val() == True:
+                    break
+        #        ledes.on()
+        #        conexionCamara, p, inputQueue, outputQueue, video_capture,nombre = rL.reconocimiento(db,llamada,indexCamara,p, inputQueue, outputQueue,video_capture, ledes, clf, pca, target_names)
+    
+                video_capture,nombre = rg.recog(im_en, target_names, db, ledes,pca,clf,video_capture)
+        #        vd.release()
+        #        ledes.off()
+                if nombre=="Desconocido":
+                    time.sleep(4)
+                elif nombre == "Sin reconocer":
+                    time.sleep(4)
+                elif nombre!="Desconocido":
+                    db.child("Habitaciones/Entrada").update({"Puerta":"Abrir"})
+                    time.sleep(15)
+        #            t0 = 0.0
+        #            if t0 == 0.0:
+        #                t0 = time.time()
+                    
+                llamada= True
+                print("valor llamada: "+ str(llamada))
+                print("Sale del reconocimiento")
+            elif nombre != "Sin reconocer" and nombre != "Desconocido":
+        
+                print("estado actual PIR")
+                print(pir.motion_detected)
+                    
+                if pir.motion_detected == False:
+                    time.sleep(5)
+                    print("Puerta cerrada")
+                    db.child("Habitaciones/Entrada").update({"Puerta":"Cerrar"})
+                    nombre = "Sin reconocer"   
+            time.sleep(1)
         
         
-
-        #Validacion para entrenar mas usuarios
-        if len(target_names)<numeroUsuarios:
-            print("se va a añadir un nuevo usuario")
-            NombresEtiquetas={}
-            for i in range(len(target_names)):
-                NombresEtiquetas[i+1] = target_names[i]
-            #Obtencion de rostros fatantes
+        else:
+            NombreCarpetaPrueba = "/home/pi/Desktop/P2/Imagenes/Proyecto del " + time.strftime("%Y_%B_%d") + "_" + time.strftime('%H_%M_%S')
+            #NombreCarpetaPrueba = "/home/pi/Desktop/P2/Prue/2018_October_11_16_49_11/"
+            pathlib.Path(NombreCarpetaPrueba).mkdir(parents=True, exist_ok=True)
             try:
-                errorObtencion, NombreCarpetaPrueba, nombreUsuarios, NombresEtiquetas, video_capture, indexCamara,numeroMuestrasRostros= obtenerRostros(indexCamara, list(target_names),len(target_names)+1,NombresEtiquetas)
-            # Envio de mensaje de error <-------------------
+                errorObtencion = True
+                targetnames = []
+                NombresEtiquetas={}
+                numeroUsuario=1
+                errorObtencion, NombreCarpetaPrueba, nombreUsuarios, NombresEtiquetas, video_capture, indexCamara,numeroMuestrasRostros= obtenerRostros(indexCamara, targetnames, numeroUsuario, NombresEtiquetas)
+                # Envio de mensaje de error <-------------------
                 db.child("Facial").update({"Error":"NoErrorCaptura"})    
+                
                 
             except:
                 
@@ -192,119 +287,27 @@ while True:
                 # Envio de mensaje de error <-------------------
                 db.child("Facial").update({"Error":"Captura"})    
                 
-            detener= db.child("Facial/Detener").get()
-            if detener.val() == True:
-                break
+            if errorObtencion ==False:
+    #            try:
+                print("ruta carpeta imagenes: "+NombreCarpetaPrueba)
+                vR.filtrar(NombreCarpetaPrueba,len(nombreUsuarios))
+    #            NombreCarpetaPrueba = "/home/pi/Desktop/P2/CaptureSVMReco/"
+                try:
+                    svm.SVM(NombreCarpetaPrueba,nombreUsuarios,numeroMuestrasRostros)
+                    # Envio de mensaje de error <-------------------
+                    print("Entrenamiento realizado correctamente")
+                    
+                    db.child("Facial").update({"Error":"NoErrorTrain"})    
+                except:
+                    # Envio de mensaje de error <-------------------
+                    print("Fallo entrenamiento")
+                    db.child("Facial").update({"Error":"Train"})    
+                print("Termino modelo")
+                print("Coninua con identifcacion de rostros")
+                db.child("Facial").update({"Configurado":True})
                 
-            # Entrenamiento de rostros
-            vR.filtrar(NombreCarpetaPrueba,numeroUsuarios)
-#            NombreCarpetaPrueba = "/home/pi/Desktop/P2/CaptureSVMReco/"
-            detener= db.child("Facial/Detener").get()
-            if detener.val() == True:
-                break
-            svm.SVM(NombreCarpetaPrueba,nombreUsuarios,numeroMuestrasRostros)
-            detener= db.child("Facial/Detener").get()
-            if detener.val() == True:
-                break
-            print("Extraccion de modelo con nuevos usuarios")
-            tomaDatos = open("archivo_modelo_LBP.pickle", "rb")
-            datos = pickle.load(tomaDatos)
-            clf = datos["modelo"]
-            pca = datos["pca"]
-            target_names = datos["target_names"]
-            NombreCarpetaPrueba = datos["NombreCarpetaPrueba"]
-
-            im_en = rg.encode(NombreCarpetaPrueba,numeroUsuarios)
-        
-        print("Clasificación de rostros")
-        # Obtener Discriminantes
-        usuariosActivados = db.child("Facial/UsuariosActivados").get()
-        usuariosActivados = usuariosActivados.val()
-        usuariosActivados = list(usuariosActivados)
-        discriminantes=[]
-        target_names = list(target_names)
-        # Comparacion de metodos
-        for i in target_names:
-            if i not in usuariosActivados:
-                discriminantes.append(i)
-        if pir.motion_detected == True and (nombre =="Desconocido" or nombre == "Sin reconocer"):
-            detener= db.child("Facial/Detener").get()
-            if detener.val() == True:
-                break
-    #        ledes.on()
-    #        conexionCamara, p, inputQueue, outputQueue, video_capture,nombre = rL.reconocimiento(db,llamada,indexCamara,p, inputQueue, outputQueue,video_capture, ledes, clf, pca, target_names)
-
-            video_capture,nombre = rg.recog(im_en, target_names, db, ledes,pca,clf,video_capture)
-    #        vd.release()
-    #        ledes.off()
-            if nombre=="Desconocido":
-                time.sleep(4)
-            elif nombre == "Sin reconocer":
-                time.sleep(4)
-            elif nombre!="Desconocido":
-                db.child("Habitaciones/Entrada").update({"Puerta":"Abrir"})
-                time.sleep(15)
-    #            t0 = 0.0
-    #            if t0 == 0.0:
-    #                t0 = time.time()
-                
-            llamada= True
-            print("valor llamada: "+ str(llamada))
-            print("Sale del reconocimiento")
-        elif nombre != "Sin reconocer" and nombre != "Desconocido":
-    
-            print("estado actual PIR")
-            print(pir.motion_detected)
-                
-            if pir.motion_detected == False:
-                time.sleep(5)
-                print("Puerta cerrada")
-                db.child("Habitaciones/Entrada").update({"Puerta":"Cerrar"})
-                nombre = "Sin reconocer"   
-        time.sleep(1)
-    
-    
-    else:
-        NombreCarpetaPrueba = "/home/pi/Desktop/P2/Imagenes/Proyecto del " + time.strftime("%Y_%B_%d") + "_" + time.strftime('%H_%M_%S')
-        #NombreCarpetaPrueba = "/home/pi/Desktop/P2/Prue/2018_October_11_16_49_11/"
-        pathlib.Path(NombreCarpetaPrueba).mkdir(parents=True, exist_ok=True)
-        try:
-            errorObtencion = True
-            targetnames = []
-            NombresEtiquetas={}
-            numeroUsuario=1
-            errorObtencion, NombreCarpetaPrueba, nombreUsuarios, NombresEtiquetas, video_capture, indexCamara,numeroMuestrasRostros= obtenerRostros(indexCamara, targetnames, numeroUsuario, NombresEtiquetas)
-            # Envio de mensaje de error <-------------------
-            db.child("Facial").update({"Error":"NoErrorCaptura"})    
-            
-            
-        except:
-            
-            print("Fallo en metodo de obtencion de rostros")
-            # Envio de mensaje de error <-------------------
-            db.child("Facial").update({"Error":"Captura"})    
-            
-        if errorObtencion ==False:
-#            try:
-            print("ruta carpeta imagenes: "+NombreCarpetaPrueba)
-            vR.filtrar(NombreCarpetaPrueba,len(nombreUsuarios))
-#            NombreCarpetaPrueba = "/home/pi/Desktop/P2/CaptureSVMReco/"
-            try:
-                svm.SVM(NombreCarpetaPrueba,nombreUsuarios,numeroMuestrasRostros)
-                # Envio de mensaje de error <-------------------
-                print("Entrenamiento realizado correctamente")
-                
-                db.child("Facial").update({"Error":"NoErrorTrain"})    
-            except:
-                # Envio de mensaje de error <-------------------
-                print("Fallo entrenamiento")
-                db.child("Facial").update({"Error":"Train"})    
-            print("Termino modelo")
-            print("Coninua con identifcacion de rostros")
-            db.child("Facial").update({"Configurado":True})
             
         
-    
 
-
-
+if __name__ == "__main__":
+    funcionPrincipal()
